6e5aeb81d2658e4e004135703e693269a914b309
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-02-10 11:34:14 +0300

    .

--- com.tugalsan.app.paint-client/src/main/java/com/tugalsan/app/paint/AppModuleCanvas.java
+++ com.tugalsan.app.paint-client/src/main/java/com/tugalsan/app/paint/AppModuleCanvas.java
@@ -2,7 +2,6 @@ package com.tugalsan.app.paint;
 
 import com.google.gwt.user.client.ui.*;
 import com.tugalsan.api.file.html.client.*;
-import com.tugalsan.api.function.client.TGS_Func;
 import com.tugalsan.api.gui.client.click.*;
 import com.tugalsan.api.gui.client.focus.*;
 import com.tugalsan.api.gui.client.key.*;
@@ -73,7 +72,7 @@ public class AppModuleCanvas extends TGC_LibBootModulePanel {
     public void configInit() {
     }
 
-    final TGS_Func onEcho = () -> {
+    final TGS_FuncMTUCE onEcho = () -> {
         TGC_SGWTCalller.async(new AppSGFEcho(tbEchoInput.getText()), reply -> {
             d.cr("onEcho", "repy", reply.getOutput_msg());
         });
